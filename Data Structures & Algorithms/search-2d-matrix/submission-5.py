fab4eccac46e6b3ec6afef1f838dbd490cfd1be6
acsahl <90639690+acsahl@users.noreply.github.com>
2026-08-19 18:27:58 -0400

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # binary search 
+        # flatten out into single long array 
+        nr = len(matrix)
+        nc = len(matrix[0])
+        lo = 0
+        hi = (nr * nc) - 1 
+        while(lo <= hi):
+            # account for overflow?
+            mid = lo + ((hi - lo) // 2)
+            # cookie buckets 
+            midVal = matrix[mid//nc][mid%nc]
+            if (midVal == target):
+                return True
+            elif (midVal < target):
+                lo = mid + 1
+            else:
+                hi = mid - 1 
+        return False
+        
