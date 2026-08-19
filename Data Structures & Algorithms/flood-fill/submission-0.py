fc9b07d916b6520078902f308913e01d4439b489
acsahl <90639690+acsahl@users.noreply.github.com>
2026-08-19 19:41:56 -0400

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        ogCol = image[sr][sc]
+        def change(sr,sc,ogCol):
+            if sr < 0 or sc < 0 or sr > (len(image)-1) or sc > (len(image[0])-1) or image[sr][sc] != ogCol:
+                return None
+            image[sr][sc] = color
+            #up
+            change(sr-1,sc,ogCol)
+            #down
+            change(sr+1,sc,ogCol)
+            #left
+            change(sr,sc-1,ogCol)
+            #right 
+            change(sr,sc+1,ogCol)
+        if ogCol == color:
+            return image
+        change(sr,sc,ogCol)
+        return image
